464fa4d07c1a26fa9e8d9dcaba9ab6d38b9a4ccb
Dylan Vassallo <dylanvassallo@gmail.com>
2013-04-29 22:11:19 -0700

    Flesh out the Express app to serve static files and render Underscore templates

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-var express = require("express");
+var consolidate = require("consolidate"),
+    express     = require("express");
 
 var app = express();
 
+app.use(express.static(__dirname + "/public"));
+app.engine("html", consolidate.underscore);
+
 app.get("/", function(req, res) {
-    res.send("Hello, world");
+    res.render("index.html", {message: "Hello, world"});
 });
 
 app.listen(3000);
