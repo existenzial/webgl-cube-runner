fa7daf4bd0728b875064c100f5815c7632de978e
Dylan Vassallo <dylanvassallo@gmail.com>
2013-04-29 22:25:47 -0700

    Split into two main views, controller and game, and route based on user agent string sniffing

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var consolidate = require("consolidate"),
-    express     = require("express");
+    express     = require("express"),
+    mobile      = require("connect-mobile-detection");
 
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
 app.engine("html", consolidate.underscore);
 
-app.get("/", function(req, res) {
-    res.render("index.html", {message: "Hello, world"});
+app.get("/", mobile(), function(req, res) {
+    if (req.mobile) {
+        res.render("controller.html", {message: "controller"});
+    } else {
+        res.render("game.html", {message: "game"});
+    }
 });
 
 app.listen(3000);
