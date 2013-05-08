0234dc086a169df4e83be25afd79d3418eef65db
Dylan Vassallo <dylanvassallo@gmail.com>
2013-05-08 13:16:38 -0700

    Display and publish accelerometer data from the controller
    alpha: compass direction in degrees
    beta: front-to-back tilt in degrees, front is positive
    gamma: left-to-right tilt in degrees, right is positive
    
    TODO: Needs to be throttled or debounced

--- public/js/controller.js
+++ public/js/controller.js
@@ -22,6 +22,7 @@ var App = {
         var self = this;
 
         self.statusView = new StatusView({el: $("#status-view")});
+        self.orientationMessage = _.template("alpha: <%= alpha %><br/>beta: <%= beta %><br/>gamma: <%= gamma %>");
 
         if (!window.DeviceOrientationEvent) {
             self.statusView.update(self.statusView.ERROR, "unsupported");
@@ -29,21 +30,25 @@ var App = {
         }
 
         self.pubsubClient = new Faye.Client("/pubsub");
-        self.pubsubSubscription = self.pubsubClient.subscribe("/game", self.pubsubMessage);
+        self.pubsubConnected = true;
+        self.pubsubSubscription = self.pubsubClient.subscribe("/game", self.onPubsubMessage);
         self.statusView.update(self.statusView.states.WARNING, "subscribing");
 
         self.pubsubSubscription.callback(function() {
             self.statusView.update(self.statusView.states.SUCCESS, "subscribed");
 
             self.pubsubClient.bind("transport:up", function() {
+                self.pubsubConnected = true;
                 self.statusView.update(self.statusView.states.SUCCESS, "subscribed");
             });
 
             self.pubsubClient.bind("transport:down", function() {
+                self.pubsubConnected = false;
                 self.statusView.update(self.statusView.states.ERROR, "disconnected");
             });
 
-            // ...
+            $(window).on("deviceorientation", _.bind(self.onDeviceOrientationEvent, self));
+            self.statusView.update(self.statusView.states.SUCCESS, "listening");
         });
 
         self.pubsubSubscription.errback(function() {
@@ -51,8 +56,22 @@ var App = {
         });
     },
 
-    pubsubMessage: function(message) {
+    onPubsubMessage: function(message) {
         console.log("pubsub: " + message);
+    },
+
+    onDeviceOrientationEvent: function(event) {
+        var data = _.pick(event.originalEvent, "alpha", "beta", "gamma");
+        if (_.contains(_.values(data), null)) { return; }
+        var displayData = data;
+        _.each(displayData, function(value, key, object) {
+            object[key] = parseFloat(value).toFixed(1);
+        });
+        console.log("orientation: " + JSON.stringify(displayData));
+        if (this.pubsubConnected) {
+            this.statusView.update(this.statusView.states.SUCCESS, this.orientationMessage(displayData));
+        }
+        this.pubsubClient.publish("/controller", data);
     }
 };
 
