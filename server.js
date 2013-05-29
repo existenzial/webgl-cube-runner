9cefbaa8c48d25caba0b2cab2fd7a63fc169c15c
Dylan Vassallo <dylanvassallo@gmail.com>
2013-05-29 12:16:00 -0700

    Remove mobile detection

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var consolidate = require("consolidate"),
     express     = require("express"),
     faye        = require("faye"),
-    mobile      = require("connect-mobile-detection"),
     os          = require("os");
 
 var app = express();
@@ -9,10 +8,6 @@ var app = express();
 app.set("port", 3000);
 app.use(express.static(__dirname + "/public"));
 app.engine("html", consolidate.underscore);
-
-app.get("/", mobile(), function(req, res) {
-    res.redirect(req.mobile ? "/controller" : "/game");
-});
 
 app.get("/controller", function(req, res) {
     res.render("controller.html", {});
